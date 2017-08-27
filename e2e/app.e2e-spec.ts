e9e5de42a4adbefb7e3f2e59421e522fa6bbacab
Tzu-Chi Yen <junipertcy@gmail.com>
2017-08-28 04:09:13 +0800

    deployed to aws on Aug 25

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('testt App', () => {
+describe('netscied App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  downloadEnglishVersion() {
+    window.location.href = 'https://sites.google.com/a/binghamton.edu/netscied/Network-Literacy-high-res.pdf';
+  }
+  downloadChineseVersion() {
+    window.location.href = 'http://wiki.junipertcy.info/images/c/ce/Network_Literacy_tw_Mandarin.pdf';
+  }
+  gotoJinMa() {
+    window.location.href = 'https://jinma.today/relations';
+  }
+}
+
+export class NzDemoFormInlineComponent implements OnInit {
+  validateForm: FormGroup;
+
+  _submitForm() {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[ i ].markAsDirty();
+    }
+  }
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.validateForm = this.fb.group({
+      userName: [ null, [ Validators.required ] ],
+      password: [ null, [ Validators.required ] ]
+    });
+  }
 }
